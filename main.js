1beb6fe4b628ea7a0a92ee51bcedfe176156e111
Malcolm Carroll <malcolmt2002@gmail.com>
2024-05-14 14:51:42 -0400

    Button QOL changes

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+// main.js
 function openInfo(infoName, elmnt) {
     var i, info, tablinks;
     info = document.getElementsByClassName("info");
+    tablinks = document.getElementsByClassName("tablink");
+
     for (i = 0; i < info.length; i++) {
+        if (info[i].style.display === "block" && elmnt.style.backgroundColor === "#888") {
+            info[i].style.display = "none";
+            elmnt.style.backgroundColor = "";
+            return;
+        }
         info[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
+
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
+
     document.getElementById(infoName).style.display = "block";
     elmnt.style.backgroundColor = "#888";
 }
