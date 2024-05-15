31ef508b7f17e135fc1646e0a1fdb24323718142
Malcolm Carroll <malcolmt2002@gmail.com>
2024-05-15 12:19:19 -0400

    Button Updates

--- main.js
+++ main.js
@@ -5,18 +5,13 @@ function openInfo(infoName, elmnt) {
     tablinks = document.getElementsByClassName("tablink");
 
     for (i = 0; i < info.length; i++) {
-        if (info[i].style.display === "block" && elmnt.style.backgroundColor === "#888") {
+        if (info[i].style.display === "block" && elmnt.style.backgroundColor === "rgb(136, 136, 136)") {
             info[i].style.display = "none";
-            elmnt.style.backgroundColor = "";
+            elmnt.style.backgroundColor = "#555";
             return;
         }
         info[i].style.display = "none";
     }
-
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
     document.getElementById(infoName).style.display = "block";
     elmnt.style.backgroundColor = "#888";
 }
