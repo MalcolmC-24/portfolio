1a29e80c1e38b3553a34e7067771bde67278fc09
Malcolm Carroll <malcolmt2002@gmail.com>
2024-05-17 01:27:07 -0400

    Button Styling and Updated About Me

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 // main.js
 function openProjects(infoName, elmnt) {
-    var i, info, tablinks;
+    var i, info, tablinks, t;
     info = document.getElementsByClassName("projects");
     tablinks = document.getElementsByClassName("tablink");
+    t = false;
 
     for (i = 0; i < info.length; i++) {
+        if (tablinks[i] === elmnt && document.getElementById(infoName).style.display === "block") {
+            info[i].style.display = "none";
+            elmnt.style.backgroundColor = "#04AA6D"; // Reset the button color
+            t = true;
+        }
+    }
+
+    for (i = 0; i < info.length; i++) {
+        tablinks[i].style.backgroundColor = "#04AA6D";
         info[i].style.display = "none";
     }
-
-    for (i = 0; i < info.length; i++) {
-        if (tablinks[i] === elmnt && tablinks[i].style.backgroundColor === "rgb(136, 136, 136)") {
-            tablinks[i].style.backgroundColor = "#555";
-            return;
-        }
-        tablinks[i].style.backgroundColor = "#555";
+    if (t) {
+        return;
     }
-
     document.getElementById(infoName).style.display = "block";
-    elmnt.style.backgroundColor = "#888";
+    elmnt.style.backgroundColor = "#3e8e41"; // Set the button color when pressed
 }
 
 function openSkills(infoName, elmnt) {
     var i, info, tablinks;
     info = document.getElementsByClassName("skills");
     tablinks = document.getElementsByClassName("tablink");
+    t = false;
 
+    for (i = 4; i < tablinks.length; i++) {
+        if (tablinks[i] === elmnt && document.getElementById(infoName).style.display === "block") {
+            info[i].style.display = "none";
+            elmnt.style.backgroundColor = "#04AA6D"; // Reset the button color
+            t = true;
+        }
+    }
+ 
     for (i = 0; i < info.length; i++) {
+        tablinks[i].style.backgroundColor = "#04AA6D";
         info[i].style.display = "none";
     }
 
-    for (i = 4; i < tablinks.length; i++) {
-        if (tablinks[i] === elmnt && tablinks[i].style.backgroundColor === "rgb(136, 136, 136)") {
-            tablinks[i].style.backgroundColor = "#555";
-            return;
-        }
-        tablinks[i].style.backgroundColor = "#555";
+    if (t) {
+        return;
     }
 
     document.getElementById(infoName).style.display = "block";
-    elmnt.style.backgroundColor = "#888";
+    elmnt.style.backgroundColor = "#3e8e41"; // Set the button color when pressed
+
 }
